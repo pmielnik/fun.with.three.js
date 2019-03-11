26c7d3c9602ce1c46b4f79c2e39d0b727edd699a
pmielnik <patricia.mielnik1@gmail.com>
2019-03-11 00:31:34 -0400

    playing around with blob.js

--- js/blob.js
+++ js/blob.js
@@ -1,4 +1,6 @@
-// renderer
+// basically a modification of this codepen: 
+// https://codepen.io/farisk/pen/vrbzwL?editors=0010
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor( 0xFFFFFF );
 renderer.setPixelRatio( window.devicePixelRatio );
@@ -16,25 +18,37 @@ var sphereMaterial = new THREE.MeshNormalMaterial();
 var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
 scene.add( sphere );
 
-var morph = function() {
+var smallSphereGeometry = new THREE.SphereGeometry( 10, 128, 128 );
+var smallSphere = new THREE.Mesh( smallSphereGeometry, sphereMaterial );
+smallSphere.position.x += 2;
+scene.add( smallSphere );
+
+var smallSphere2Geometry = new THREE.SphereGeometry( 10, 128, 128 );
+var smallSphere2 = new THREE.Mesh( smallSphere2Geometry, sphereMaterial );
+smallSphere2.position.x -= 2;
+scene.add( smallSphere2 );
+
+var morph = function( shape, size, spikes ){
     // change the end value to increase/decrease morph speed
     var time = performance.now() * 0.0005;
     
     // change 'k' value for more spikes
-    var k = 2;
-    for (var i = 0; i < sphere.geometry.vertices.length; i++) {
-        var v = sphere.geometry.vertices[i];
-        v.normalize().multiplyScalar( 1 + 0.5 * noise.perlin3( v.x * k + time, v.y * k, v.z * k ));
+    var k = spikes;
+    for (var i = 0; i < shape.geometry.vertices.length; i++) {
+        var v = shape.geometry.vertices[i];
+        v.normalize().multiplyScalar( size + size*0.5 * noise.perlin3( v.x * k + time, v.y * k, v.z * k ));
     }
-    sphere.geometry.computeVertexNormals();
-    sphere.geometry.normalsNeedUpdate = true;
-    sphere.geometry.verticesNeedUpdate = true;
+    shape.geometry.computeVertexNormals();
+    shape.geometry.normalsNeedUpdate = true;
+    shape.geometry.verticesNeedUpdate = true;
 }
 
 function animate() {
     requestAnimationFrame( animate );
 
-    morph();
+    morph(sphere, 1, 2);                // medium spikes
+    morph(smallSphere, 0.25, 1);        // few spikes
+    morph(smallSphere2, 0.25, 3);       // very spiky
     renderer.render( scene,camera );
 }
 
